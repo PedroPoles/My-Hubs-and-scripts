2932bc3fd709b02798d4f8a736cdaeb697d94add
PedroPoles <pedroopssss@gmail.com>
2025-09-20 14:22:52 -0300

    Add files via upload

--- P3dr0PolesHubV1.lua
+++ P3dr0PolesHubV1.lua
@@ -1,5 +1,7 @@
+-- Carregar Rayfield
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
+-- Criar janela principal
 local Window = Rayfield:CreateWindow({
    Name = "P3dr0Poles Hub V1",
    Icon = 0,
@@ -37,14 +39,14 @@ local Window = Rayfield:CreateWindow({
    }
 })
 
--- =========================
--- ABA PLAYER
--- =========================
-local PlayerTab = Window:CreateTab("Player", 4483362458)
-local PlayerSection = PlayerTab:CreateSection("Player Options")
+---------------------------------------------------
+-- Aba Player
+---------------------------------------------------
+local Tab = Window:CreateTab("Player", 4483362458)
+local Section = Tab:CreateSection("Player Options")
 
 -- WalkSpeed
-PlayerTab:CreateSlider({
+Tab:CreateSlider({
    Name = "WalkSpeed",
    Range = {16, 200},
    Increment = 1,
@@ -57,7 +59,7 @@ PlayerTab:CreateSlider({
 })
 
 -- JumpPower
-PlayerTab:CreateSlider({
+Tab:CreateSlider({
    Name = "JumpPower",
    Range = {50, 300},
    Increment = 10,
@@ -71,7 +73,7 @@ PlayerTab:CreateSlider({
 
 -- Infinite Jump
 local InfiniteJump = false
-PlayerTab:CreateToggle({
+Tab:CreateToggle({
    Name = "Infinite Jump",
    CurrentValue = false,
    Flag = "InfiniteJumpToggle",
@@ -91,7 +93,7 @@ end)
 
 -- NoClip
 local NoClip = false
-PlayerTab:CreateToggle({
+Tab:CreateToggle({
    Name = "NoClip",
    CurrentValue = false,
    Flag = "NoClipToggle",
@@ -116,7 +118,8 @@ end)
 -- Fly
 local Fly = false
 local FlySpeed = 3
-PlayerTab:CreateInput({
+
+Tab:CreateInput({
    Name = "Fly Speed",
    PlaceholderText = "Digite a velocidade",
    RemoveTextAfterFocusLost = true,
@@ -127,7 +130,8 @@ PlayerTab:CreateInput({
       end
    end,
 })
-PlayerTab:CreateToggle({
+
+Tab:CreateToggle({
    Name = "Fly",
    CurrentValue = false,
    Flag = "FlyToggle",
@@ -144,73 +148,173 @@ local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
 RS.RenderStepped:Connect(function()
    if Fly then
-      local moveVector = Vector3.new(0,0,0)
-      if UIS:IsKeyDown(Enum.KeyCode.W) then moveVector = moveVector + (workspace.CurrentCamera.CFrame.LookVector * FlySpeed) end
-      if UIS:IsKeyDown(Enum.KeyCode.S) then moveVector = moveVector - (workspace.CurrentCamera.CFrame.LookVector * FlySpeed) end
-      if UIS:IsKeyDown(Enum.KeyCode.A) then moveVector = moveVector - (workspace.CurrentCamera.CFrame.RightVector * FlySpeed) end
-      if UIS:IsKeyDown(Enum.KeyCode.D) then moveVector = moveVector + (workspace.CurrentCamera.CFrame.RightVector * FlySpeed) end
-      if UIS:IsKeyDown(Enum.KeyCode.Space) then moveVector = moveVector + Vector3.new(0, FlySpeed, 0) end
-      if UIS:IsKeyDown(Enum.KeyCode.LeftShift) then moveVector = moveVector - Vector3.new(0, FlySpeed, 0) end
+      local moveVector = Vector3.new(0, 0, 0)
+      if UIS:IsKeyDown(Enum.KeyCode.W) then
+         moveVector = moveVector + (workspace.CurrentCamera.CFrame.LookVector * FlySpeed)
+      end
+      if UIS:IsKeyDown(Enum.KeyCode.S) then
+         moveVector = moveVector - (workspace.CurrentCamera.CFrame.LookVector * FlySpeed)
+      end
+      if UIS:IsKeyDown(Enum.KeyCode.A) then
+         moveVector = moveVector - (workspace.CurrentCamera.CFrame.RightVector * FlySpeed)
+      end
+      if UIS:IsKeyDown(Enum.KeyCode.D) then
+         moveVector = moveVector + (workspace.CurrentCamera.CFrame.RightVector * FlySpeed)
+      end
+      if UIS:IsKeyDown(Enum.KeyCode.Space) then
+         moveVector = moveVector + Vector3.new(0, FlySpeed, 0)
+      end
+      if UIS:IsKeyDown(Enum.KeyCode.LeftShift) then
+         moveVector = moveVector - Vector3.new(0, FlySpeed, 0)
+      end
       humanoidRootPart.Velocity = moveVector * 10
    end
 end)
 
--- =========================
--- ABA FPS
--- =========================
-local FPSTab = Window:CreateTab("FPS", 4483362458)
-local FPSSection = FPSTab:CreateSection("Funções FPS")
+---------------------------------------------------
+-- Aba FPS
+---------------------------------------------------
+local FpsTab = Window:CreateTab("FPS", 4483362458)
 
--- Toggle Aimbot
-FPSTab:CreateToggle({
-    Name = "Aimbot",
-    CurrentValue = false,
-    Flag = "AimbotToggle",
-    Callback = function(Value)
-        if Value then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()()
-        end
-    end
+FpsTab:CreateToggle({
+   Name = "Aimbot",
+   CurrentValue = false,
+   Flag = "AimbotToggle",
+   Callback = function(Value)
+      if Value then
+         loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()()
+      end
+   end,
 })
 
--- Toggle ESP
-FPSTab:CreateToggle({
-    Name = "ESP",
-    CurrentValue = false,
-    Flag = "ESPToggle",
-    Callback = function(Value)
-        if Value then
-            _G.Settings = {ShowNames = true, ShowDistance = true}
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/apakekbebas/esp-lib/main/lib.lua", true))()
-        end
-    end
+FpsTab:CreateToggle({
+   Name = "ESP",
+   CurrentValue = false,
+   Flag = "EspToggle",
+   Callback = function(Value)
+      if Value then
+         _G.Settings = {ShowNames = true, ShowDistance = true}
+         loadstring(game:HttpGet("https://raw.githubusercontent.com/apakekbebas/esp-lib/main/lib.lua", true))()
+      end
+   end,
 })
 
--- =========================
--- ABA ADMIN
--- =========================
+---------------------------------------------------
+-- Aba Admin
+---------------------------------------------------
 local AdminTab = Window:CreateTab("Admin", 4483362458)
-local AdminSection = AdminTab:CreateSection("Admin Scripts")
 
-AdminTab:CreateButton({Name="Infinite Yield",Callback=function()loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()end})
-AdminTab:CreateButton({Name="AK / Angelical",Callback=function()loadstring(game:HttpGet("https://angelical.me/ak.lua"))()end})
-AdminTab:CreateButton({Name="Fates Admin",Callback=function()loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))()end})
-AdminTab:CreateButton({Name="Cmd-X",Callback=function()loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()end})
-AdminTab:CreateButton({Name="Leg Admin V2",Callback=function()loadstring(game:HttpGet('https://raw.githubusercontent.com/leg1337/legadmv2/main/legadminv2.lua'))()end})
-AdminTab:CreateButton({Name="Homebrew Admin",Callback=function()loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Syntaxx64/HomebrewAdmin/master/Main"))()end})
-AdminTab:CreateButton({Name="Reviz Admin",Callback=function()loadstring(game:HttpGetAsync("https://pastebin.com/raw/gQg0G6iA"))()end})
-AdminTab:CreateButton({Name="GX Admin",Callback=function()loadstring(game:HttpGet("https://pastebin.com/raw/v6E9BmFK", true))()end})
-AdminTab:CreateButton({Name="IV Admin",Callback=function()loadstring(game:HttpGet('https://raw.githubusercontent.com/BloodyBurns/Hex/main/Iv%20Admin%20v3.lua'))()end})
+AdminTab:CreateButton({
+   Name = "Infinite Yield",
+   Callback = function()
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+   end,
+})
 
--- =========================
--- ABA HUBS
--- =========================
+AdminTab:CreateButton({
+   Name = "AK Admin",
+   Callback = function()
+      loadstring(game:HttpGet("https://angelical.me/ak.lua"))()
+   end,
+})
+
+AdminTab:CreateButton({
+   Name = "Fates Admin",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))()
+   end,
+})
+
+AdminTab:CreateButton({
+   Name = "CmdX",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
+   end,
+})
+
+AdminTab:CreateButton({
+   Name = "Leg Admin V2",
+   Callback = function()
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/leg1337/legadmv2/main/legadminv2.lua'))()
+   end,
+})
+
+AdminTab:CreateButton({
+   Name = "Homebrew Admin",
+   Callback = function()
+      loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Syntaxx64/HomebrewAdmin/master/Main"))()
+   end,
+})
+
+AdminTab:CreateButton({
+   Name = "Reviz Admin",
+   Callback = function()
+      loadstring(game:HttpGetAsync("https://pastebin.com/raw/gQg0G6iA"))()
+   end,
+})
+
+AdminTab:CreateButton({
+   Name = "GX Admin",
+   Callback = function()
+      loadstring(game:HttpGet("https://pastebin.com/raw/v6E9BmFK",true))()
+   end,
+})
+
+---------------------------------------------------
+-- Aba Hubs
+---------------------------------------------------
 local HubsTab = Window:CreateTab("Hubs", 4483362458)
-local HubsSection = HubsTab:CreateSection("Outras Hubs")
+
+-- Spanish Hub
+local SpanishHubSection = HubsTab:CreateSection("Spanish Hub")
 
 HubsTab:CreateButton({
-    Name = "INDRIPRISXTENXXS Admin Hub V8",
-    Callback = function()
-        loadstring(game:HttpGet("https://pastebin.com/raw/AHv9tabE"))()
-    end
+   Name = "Spanish Hub (Script 1)",
+   Callback = function()
+      loadstring(game:HttpGet("https://paste.myconan.net/499233.txt"))()
+   end,
+})
+
+HubsTab:CreateButton({
+   Name = "Spanish Hub (Script 2)",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/gclich/GHUBV14XZ/main/Ghub_Main_Loader.txt"))()
+   end,
+})
+
+-- Game Hub
+local GameHubSection = HubsTab:CreateSection("Game Hub")
+
+HubsTab:CreateButton({
+   Name = "Game Hub (Loader 1)",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/GamerScripter/Game-Hub/main/loader"))()
+   end,
+})
+
+HubsTab:CreateButton({
+   Name = "Game Hub (Loader 2)",
+   Callback = function()
+      loadstring(game:HttpGet("https://paste.myconan.net/489718.txt"))()
+   end,
+})
+
+-- Other Hubs
+local OtherHubSection = HubsTab:CreateSection("Other Hubs")
+
+HubsTab:CreateButton({
+   Name = "GHUB X ZENXOS V15",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/gclich/GHUBV15_X_ZENXOS-MAINLOADER/refs/heads/main/GHUB-X-ZENXOS-V15.txt"))()
+   end,
+})
+
+-- Animations Hub
+local AnimationsHubSection = HubsTab:CreateSection("Animations Hub")
+
+HubsTab:CreateButton({
+   Name = "AFEM Max (All UGC Emotes)",
+   Callback = function()
+      loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-AFEM-Max-ALL-UGC-EMOTES-FREE-FE-NEW-53663"))()
+   end,
 })
